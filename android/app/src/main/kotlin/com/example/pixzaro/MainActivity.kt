5dfe10ffc579d09f32ab39943ba01f32b4f07464
kabilan386 <kabilankabi386@gmail.com>
2022-12-08 21:35:53 +0530

    first commit

--- android/app/src/main/kotlin/com/example/pixzaro/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pixzaro
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/Logout.dart
+++
@@ -1,6 +0,0 @@
-  // import 'package:flutter/material.dart';
-  // signOut() async {
-  //   // await auth.signOut();
-  //   Navigator.pushReplacement(
-  //       context, MaterialPageRoute(builder: (context) => SignInScreen()));
-  // }
